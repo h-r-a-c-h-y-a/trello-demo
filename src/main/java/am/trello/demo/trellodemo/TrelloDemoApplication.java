81b3f6cb8d4e45f12c365f62aef7c9a3f047c132
h-r-a-c-h-y-a <hrachxach90@mail.ru>
2019-07-18 21:06:30 +0400

    create project trello-demo

---
+++ src/main/java/am/trello/demo/trellodemo/TrelloDemoApplication.java
@@ -0,0 +1,13 @@
+package am.trello.demo.trellodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloDemoApplication.class, args);
+    }
+
+}
