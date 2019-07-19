d34cdf1dfdec86243fa7fd2eb36a57a47347c766
h-r-a-c-h-y-a <hrachxach90@mail.ru>
2019-07-19 10:57:42 +0400

    update sql file

--- sql/trello.sql
+++ sql/trello.sql
@@ -1,1 +1,52 @@
+SET GLOBAL TIME_ZONE = '+04:00';
 CREATE DATABASE trello_demo;
+USE trello_demo;
+CREATE TABLE users
+(
+    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email     VARCHAR(255) NOT NULL UNIQUE,
+    password  VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    initial   CHAR(3)      NOT NULL,
+    image_url VARCHAR(255) DEFAULT 'images/user.png',
+    about_me  VARCHAR(255)
+);
+
+CREATE TABLE roles
+(
+    id   TINYINT PRIMARY KEY AUTO_INCREMENT,
+    role CHAR(70) NOT NULL UNIQUE DEFAULT 'USER'
+);
+
+CREATE TABLE users_roles
+(
+    user_id BIGINT  NOT NULL,
+    role_id TINYINT NOT NULL,
+    CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT role_id FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+CREATE TABLE task_bar
+(
+    id    SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    title CHAR(60) NOT NULL
+);
+
+CREATE TABLE cards
+(
+    id          SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    title       CHAR(100) NOT NULL,
+    description VARCHAR(255),
+    comment     VARCHAR(255),
+    history     VARCHAR(100)
+);
+
+
+CREATE TABLE task_bar_cards
+(
+    task_bar_id SMALLINT  NOT NULL,
+    card_id SMALLINT NOT NULL,
+    CONSTRAINT task_bar_id FOREIGN KEY (task_bar_id) REFERENCES task_bar (id),
+    CONSTRAINT card_id FOREIGN KEY (card_id) REFERENCES cards (id)
+);
