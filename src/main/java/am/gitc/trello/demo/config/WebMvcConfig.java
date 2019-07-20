d93734a08185ccba00bd9c4ca1c514e284725147
h-r-a-c-h-y-a <hrachxach90@mail.ru>
2019-07-20 12:23:16 +0400

    some changes in WebMvcConfig

--- src/main/java/am/gitc/trello/demo/config/WebMvcConfig.java
+++ src/main/java/am/gitc/trello/demo/config/WebMvcConfig.java
@@ -3,22 +3,22 @@ package am.gitc.trello.demo.config;
 import am.gitc.trello.demo.interceptor.LogInterceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Component
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final LogInterceptor logInterceptor;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     @Autowired
     private WebMvcConfig(LogInterceptor logInterceptor) {
