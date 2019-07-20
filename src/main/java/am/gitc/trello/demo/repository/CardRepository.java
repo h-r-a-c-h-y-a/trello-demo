ef2fee5aca29aeb56d51f913fe11e16100dd1937
astghik <astghikaramyan@gmail.com>
2019-07-20 10:44:56 +0400

    created repository folder and added into it CardRepo.java file

---
+++ src/main/java/am/gitc/trello/demo/repository/CardRepository.java
@@ -0,0 +1,12 @@
+package am.gitc.trello.demo.repository;
+
+import am.gitc.trello.demo.entity.CardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by User on 20.07.2019.
+ */
+@Repository
+public interface CardRepository extends JpaRepository<CardEntity, Integer> {
+}
